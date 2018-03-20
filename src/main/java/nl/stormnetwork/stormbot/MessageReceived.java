a4daf7695177441130af564d0767fc81a87df32d
TCOOfficiall <stefan0-@hotmail.com>
2018-03-20 16:48:44 +0100

    xD

--- src/main/java/nl/stormnetwork/stormbot/MessageReceived.java
+++ src/main/java/nl/stormnetwork/stormbot/MessageReceived.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import sun.plugin2.message.TextEventMessage;
 
 public class MessageReceived extends ListenerAdapter {
 
@@ -18,37 +19,35 @@ public class MessageReceived extends ListenerAdapter {
             event.getChannel().sendMessage("Het heeft (nog) geen zin om een **private** bericht te sturen.").queue();
         } else if (event.isFromType(ChannelType.TEXT)) {
 
-            Guild g = jda.getGuildById(ID.ServerID);
             TextChannel c;
-            if (g != null) {
-                c = jda.getTextChannelById(ID.ChannelID);
-                if (c != null) {
-                    Message m = event.getMessage();
-                    if (m.getChannel().getId().equals(ID.IdeenChannelID)) {
-                        m.addReaction("\uD83D\uDC4D").queue();
-                        m.addReaction("\uD83D\uDC4E").queue();
-                    }
-
-                    if (m.getContentRaw().startsWith("!forum")) {
-                        c.sendMessage("https://forum.plopgroep.ga").queue();
-                    }
-                    else if (m.getContentRaw().startsWith("!help")) {
-                        c.sendMessage("**__COMMANDS__**").queue();
-                        c.sendMessage("**!**forum").queue();
-                        c.sendMessage("**!**ip").queue();
-                        c.sendMessage("**!**help").queue();
-
-
-                    }
-                    else if (m.getContentRaw().startsWith("!ip")) {
-                        c.sendMessage("**IP:** play.stormnetwork.nl").queue();
-                    }
-
+            c = jda.getTextChannelById(ID.ChannelID);
+            if (c != null) {
+                MessageChannel m = event.getMessage().getTextChannel();
+                Message M = event.getMessage();
+                if (m.getId().equals(ID.IdeenChannelID)) {
+                    M.addReaction("\uD83D\uDC4D").queue();
+                    M.addReaction("\uD83D\uDC4E").queue();
                 }
 
+                if (M.getContentRaw().startsWith("!store")) {
+                    m.sendMessage(":large_blue_circle: Het Buycraft linkie: http://stnetwerk.buycraft.net/ :large_blue_circle:").queue();
+                } else if (M.getContentRaw().startsWith("!help")) {
+                    m.sendMessage("**__COMMANDS__**").queue();
+                    m.sendMessage("**!**store").queue();
+                    m.sendMessage("**!**ip").queue();
+                    m.sendMessage("**!**help").queue();
+
+
+                } else if (M.getContentRaw().startsWith("!ip")) {
+                    m.sendMessage(":large_blue_circle: Jordy speelt momenteel op het volgende **IP:** play.stormnetwork.nl :large_blue_circle:").queue();
+                }
+
+
             }
+
         }
-
-
     }
+
+
 }
+
